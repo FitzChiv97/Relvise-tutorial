976a8b4f205d068a44d2a7e12b6534b6c0000719
FitzChiv97 <porter.morgan.97bb@gmail.com>
2026-03-22 08:49:37 +0300

    feat: add burger menu auto-close

--- js/script.js
+++ js/script.js
@@ -1,6 +1,25 @@
 'use strict';
 
 const burgerButton = document.querySelector('.js-burger-button');
-burgerButton.addEventListener('click', () => {
-    burgerButton.parentElement.classList.toggle('active');
-});
+
+burgerButton.addEventListener('click', toggleBurgerMenu);
+
+const bodyElement = document.body;
+
+bodyElement.addEventListener('click', (e) => closeBurgerMenu(e))
+
+function toggleBurgerMenu() {
+    burgerButton.closest('.menu').classList.toggle('_active');
+    document.body.classList.toggle('_lock');
+}
+
+function closeBurgerMenu(e) {
+    const menu = document.querySelector('.menu');
+
+    if(menu.classList.contains('_active') && 
+    !e.target.closest('.menu__list') && 
+    !e.target.closest('.js-burger-button')) {
+        menu.classList.remove('_active');
+        bodyElement.classList.remove('_lock');
+    }
+}
